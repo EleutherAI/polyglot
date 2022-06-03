b2a1a44ebbce9a5c6125e4d3356ec2014015bbbb
momo <ahdbsgh2@naver.com>
2022-06-03 15:48:14 +0900

    xpt-neo add EarlyStopping

--- xpt_neo_training.py
+++ xpt_neo_training.py
@@ -12,7 +12,7 @@ from torch.utils.data import DataLoader, DistributedSampler
 from transformers import AutoTokenizer
 from wandb import Table
 
-from datasets import tqdm
+from tqdm import tqdm
 from multilingual.data.datasets.dataset_causal_lm import DatasetForCausalLM
 from multilingual.data.utils.blenders import DatasetBlender
 from multilingual.models.xpt_neo.modeling_xpt_neo import XPTNeoForCausalLM
@@ -33,6 +33,43 @@ class CheckPhase:
     def set_phase(self, current_phase):
         self.phase = current_phase
 
+
+class EarlyStopping:
+    def __init__(self, patience=3, min_delta=0.0, min_steps=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.min_steps = min_steps
+        self.min_val_loss = 999999
+        self.patience_count = 0
+
+        self.initial_patience = self.patience
+        self.initial_min_delta = self.min_delta
+        self.initial_min_steps = self.min_steps
+        self.initial_min_val_loss = self.min_val_loss
+        self.initial_patience_count = self.patience_count
+
+    def __call__(self, current_steps, valid_loss):
+        '''
+         If the early termination condition is satisfied, return True.
+         otherwise, return None
+        '''
+
+        if valid_loss - self.min_delta > self.min_val_loss:
+            if current_steps > self.min_steps:
+                self.patience_count += 1
+        elif valid_loss - self.min_delta < self.min_val_loss:
+            self.min_val_loss = valid_loss
+            self.patience_count = 0
+
+        if self.patience_count > self.patience:
+            return True # finish train
+
+    def reset_(self):
+        self.patience = self.initial_patience
+        self.min_delta = self.initial_min_delta
+        self.min_steps = self.initial_min_steps
+        self.min_val_loss = self.initial_min_val_loss
+        self.patience_count = self.initial_patience_count
 
 # Initialize program
 SEED = 42
@@ -144,10 +181,14 @@ engine, optimizer, _, _ = deepspeed.initialize(
 # Initialize wandb
 if dist.get_rank() == 0:
     wandb.init(
+        config=config,
         name=f"{config['experiment']['type']}-{config['experiment']['name']}",
         project="XPT-Training",
     )
 
+
+# Set training args
+early_stopping = EarlyStopping(patience=0, min_delta=0.0, min_steps=10000)
 
 # Start training
 wandb_generation_table = []
@@ -162,7 +203,7 @@ while True:
             if dist.get_rank() == 0:
                 phase_detector.set_phase(current_phase="phase2")
                 print("START Phase-2")
-
+        
         if CURRENT_STEP >= total_num_steps:
             break
 
@@ -186,7 +227,8 @@ while True:
             )
             print(
                 f"STEP: {CURRENT_STEP}/" f"{total_num_steps}, " f"LOSS: {loss}, ",
-                f"PPL: {ppl}",
+                f"PPL: {ppl}, ",
+                f"CURRENT PHASE: {phase_detector().upper()}"
             )
 
         engine.backward(loss)
@@ -205,9 +247,7 @@ while True:
                 else:
                     eval_total = len(valid_loader) + 1
 
-                for eval_steps, valid_data in enumerate(
-                    tqdm(valid_loader, total=eval_total)
-                ):
+                for eval_steps, valid_data in enumerate(tqdm(valid_loader, total=eval_total)):
                     if config["experiment"]["max_eval_steps"] is not None:
                         if eval_steps > config["experiment"]["max_eval_steps"]:
                             break
@@ -218,17 +258,44 @@ while True:
                         labels=valid_data,
                         use_cache=False,
                     ).loss
+
                     val_losses.append(val_loss.detach().item())
                     valid_samples.append(choice(valid_data))
 
                 val_loss = sum(val_losses) / len(val_losses)
-
                 num_prompt_tokens = 5
                 sample = choice(valid_samples)[:num_prompt_tokens]
-                generated_output = model.generate(sample.unsqueeze(0), max_length=25)
+                generated_output = model.generate(sample.unsqueeze(0), max_length=25, pad_token_id=tokenizer.eos_token_id)
                 generated_text = tokenizer.decode(generated_output[0])
+                input_prompt = tokenizer.decode(sample)
+                print(f"Input Prompt: {input_prompt}")
+                print(f"Generated sentence: {generated_text}")
                 wandb_generation_table.append(
-                    [CURRENT_STEP, tokenizer.decode(sample), generated_text]
+                    [CURRENT_STEP, input_prompt, generated_text]
+                )
+
+            if dist.get_rank() == 0:
+                val_ppl = math.exp(val_loss)
+                print("=" * 100)
+                print(
+                    f"STEP: {CURRENT_STEP}/"
+                    f"{total_num_steps}, "
+                    f"VALID_LOSS: {val_loss}, ",
+                    f"VALID_PPL: {val_ppl}, ",
+                    f"CURRENT PHASE: {phase_detector().upper()}"
+                )
+                print("=" * 100)
+
+                wandb.log(
+                    data={
+                        f"{phase_detector()}/val_loss": val_loss,
+                        f"{phase_detector()}/val_ppl": math.exp(val_loss),
+                        f"{phase_detector()}/generation": Table(
+                            columns=["Step", "Input Prompt", "Generated Text"],
+                            data=wandb_generation_table,
+                        ),
+                    },
+                    step=CURRENT_STEP,
                 )
 
                 if (
@@ -237,44 +304,44 @@ while True:
                     == 0
                     and CURRENT_STEP != 0
                 ):
+                    print("SAVE CHECKPOINTS...")
                     save_dir = os.path.join(
                         config["experiment"]["checkpoint_params"]["save_dir"],
+                        f"{phase_detector()}-"
                         f"{config['experiment']['type']}-"
                         f"{config['experiment']['name']}-"
                         f"steps={CURRENT_STEP}",
                     )
+                    model.save_pretrained(save_dir)
+            engine.save_checkpoint(os.path.join(save_dir, "deepspeed"))
 
-                if dist.get_rank() == 0:
-                    val_ppl = math.exp(val_loss)
-                    print("=" * 100)
-                    print(
-                        f"STEP: {CURRENT_STEP}/"
-                        f"{total_num_steps}, "
-                        f"LOSS: {val_loss}, ",
-                        f"PPL: {val_ppl}",
-                    )
-                    print("=" * 100)
-
-                    wandb.log(
-                        data={
-                            f"{phase_detector()}/val_loss": val_loss,
-                            f"{phase_detector()}/val_ppl": math.exp(val_loss),
-                            f"{phase_detector()}/generation": Table(
-                                columns=["Step", "Input Prompt", "Generated Text"],
-                                data=wandb_generation_table,
-                            ),
-                        },
-                        step=CURRENT_STEP,
-                    )
-
-                    if (
-                        CURRENT_STEP
-                        % config["experiment"]["checkpoint_params"]["save_interval"]
-                        == 0
-                        and CURRENT_STEP != 0
-                    ):
-                        model.save_pretrained(save_dir)
-
+            if early_stopping(current_steps=CURRENT_STEP, valid_loss=val_loss) and phase_detector() == 'phase1':
+                # finish phase 1
+                print(f"Ealry STOP in {phase_detector()}")
+                save_dir = os.path.join(
+                    config["experiment"]["checkpoint_params"]["save_dir"],
+                    f"{phase_detector()}-"
+                    f"{config['experiment']['type']}-"
+                    f"{config['experiment']['name']}-"
+                    f"steps={CURRENT_STEP}",
+                )
+                model.save_pretrained(save_dir)
                 engine.save_checkpoint(os.path.join(save_dir, "deepspeed"))
+                CURRENT_STEP = config["experiment"]["args"]["phase1_steps"]
+                early_stopping.reset_()
+
+            elif early_stopping(current_steps=CURRENT_STEP, valid_loss=val_loss) and phase_detector() == 'phase2':
+                # finish phase 2
+                print(f"Ealry STOP in {phase_detector()}")
+                save_dir = os.path.join(
+                    config["experiment"]["checkpoint_params"]["save_dir"],
+                    f"{phase_detector()}-"
+                    f"{config['experiment']['type']}-"
+                    f"{config['experiment']['name']}-"
+                    f"steps={CURRENT_STEP}",
+                )
+                model.save_pretrained(save_dir)
+                engine.save_checkpoint(os.path.join(save_dir, "deepspeed"))
+                CURRENT_STEP = total_num_steps
 
         CURRENT_STEP += 1
